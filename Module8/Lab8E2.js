3e26e4638ac378d7fb800504646a1895076e3658
Dennae13 <dennaedawk13@gmail.com>
2021-11-04 19:31:19 -0400

    Updated Lab8 E2

--- Module8/Lab8E2.js
+++ Module8/Lab8E2.js
@@ -1,17 +1,16 @@
 //Sets the initial colours of the three elements on page load
-function mix(){
 document.getElementById("blue").style.color="blue";
 document.getElementById("green").style.color="green";
 document.getElementById("red").style.color="red";
 
 //A function to change colours of elements when called
-
-	var first = "#0000FF";
-	var second = "#00FF00";
-	var third = "#FF0000";
+function mix(){
+  var first = "#00BB00";
+  var second = "BB9320";
+  var third = "#AB00FF";
 
 	document.getElementById("blue").style.color= first;
-	document.getElementById("green").style.colour= second;
+	document.getElementById("green").style.color= second;
 	document.getElementById("red").style.color= third;
 
 }
